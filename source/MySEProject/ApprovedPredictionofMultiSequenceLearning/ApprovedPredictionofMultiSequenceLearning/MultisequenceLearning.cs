0a2931cb57235bb909aa8bc7feb5781b3eb75c95
nabeelamaham <nabeela.maham@stud.fra-uas.de>
2024-01-21 16:22:30 +0100

    created class multiSequenceLearning

---
+++ source/MySEProject/ApprovedPredictionofMultiSequenceLearning/ApprovedPredictionofMultiSequenceLearning/MultisequenceLearning.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+
+namespace NeoCortexApiSample
+{
+    /// <summary>
+    /// Implements an experiment that demonstrates how to learn sequences.
+    /// </summary>
+    public class MultiSequenceLearning
+    {
+    }
+}
