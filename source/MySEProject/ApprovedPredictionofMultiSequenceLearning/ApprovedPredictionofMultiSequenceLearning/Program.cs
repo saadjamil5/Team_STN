96f316061c96c5ff3f39e0448d872ada6efb91f9
Saad Jamil <33003836+saadjamil5@users.noreply.github.com>
2024-01-21 20:55:32 +0100

    Update Program.cs

--- source/MySEProject/ApprovedPredictionofMultiSequenceLearning/ApprovedPredictionofMultiSequenceLearning/Program.cs
+++ source/MySEProject/ApprovedPredictionofMultiSequenceLearning/ApprovedPredictionofMultiSequenceLearning/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using NeoCortexApi;
+using NeoCortexApi.Encoders;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using static MultiSequenceLearning.MultiSequenceLearning;
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
