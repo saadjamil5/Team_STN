e7d6d02a3a29c301c4a04549eaee1cdb69b7f883
Saad Jamil <33003836+saadjamil5@users.noreply.github.com>
2024-01-21 20:53:33 +0100

    Update MultisequenceLearning.cs

--- source/MySEProject/ApprovedPredictionofMultiSequenceLearning/ApprovedPredictionofMultiSequenceLearning/MultisequenceLearning.cs
+++ source/MySEProject/ApprovedPredictionofMultiSequenceLearning/ApprovedPredictionofMultiSequenceLearning/MultisequenceLearning.cs
@@ -1,9 +1,11 @@
-﻿
-using System;
+﻿using System;
+using System.Globalization;
+using System.IO;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-
+using System.Text;
+using System.Threading.Tasks;
+using NeoCortexApi;
 
 namespace NeoCortexApiSample
 {
@@ -11,6 +13,11 @@ namespace NeoCortexApiSample
     /// Implements an experiment that demonstrates how to learn sequences.
     /// </summary>
     public class MultiSequenceLearning
-    {
-    }
-}
+    { /// <summary>
+        /// Runs the learning of sequences.
+        /// </summary>
+        /// <param name="sequences">Dictionary of sequences. KEY is the sewuence name, the VALUE is th elist of element of the sequence.</param>
+        public Predictor Run(List<Sequence> sequences)
+        {
+            Console.WriteLine($"Hello NeocortexApi! Experiment {nameof(MultiSequenceLearning)}");
+        }
