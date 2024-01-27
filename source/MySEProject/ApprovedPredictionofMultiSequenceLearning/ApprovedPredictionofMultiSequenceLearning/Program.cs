c5b6ee3a238b4cf7c622717153c4c153ccd3e677
TAhmed-fra <tanvir.ahmed@stud.fra-uas.de>
2024-01-27 04:41:51 +0100

    changes to multisequenceleqarning

--- source/MySEProject/ApprovedPredictionofMultiSequenceLearning/ApprovedPredictionofMultiSequenceLearning/Program.cs
+++ source/MySEProject/ApprovedPredictionofMultiSequenceLearning/ApprovedPredictionofMultiSequenceLearning/Program.cs
@@ -1,12 +1,18 @@
-﻿using NeoCortexApi;
-using NeoCortexApi.Encoders;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using static MultiSequenceLearning.MultiSequenceLearning;
+﻿
+namespace ApprovePredictionofMultiSequenceLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> sequences = new Dictionary<string, List<double>>();
 
+            //have to make class for reading data values
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+            sequences.Add("S1", new List<double>(new double[] { 0.0, 1.0, 2.0, 3.0, 4.0, 2.0, 5.0, }));
+            MultiSequenceLearning experiment = new MultiSequenceLearning();
+            var predictor = experiment.Run(sequences);
+
+        }
+    }
+}
