46353e2c42bfa83dcc9294b2098bca558bc18456
nabeelamaham <nabeela.maham@stud.fra-uas.de>
2024-01-30 22:18:12 +0100

    reading dataset

---
+++ source/MySEProject/MultisequenceLearning/MultisequenceLearning/Program.cs
@@ -0,0 +1,41 @@
+﻿using MultisequenceLearning;
+using NeoCortexApi;
+using NeoCortexApi.Encoders;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using static MultiSequenceLearning.MultiSequenceLearning;
+
+namespace MultiSequenceLearning
+{
+    class Program
+    {
+        private static object HelperMethods;
+
+        /// <summary>
+        /// This sample shows a typical experiment code for SP and TM.
+        /// You must start this code in debugger to follow the trace.
+        /// and TM.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // SE Project: ML22/23-15	Approve Prediction of Multisequence Learning 
+
+            //to create synthetic dataset
+            /*string path = HelperMethods.SaveDataset(HelperMethods.CreateDataset());
+            Console.WriteLine($"Dataset saved: {path}");*/
+
+            //to read dataset
+            string BasePath = AppDomain.CurrentDomain.BaseDirectory;
+            string datasetPath = Path.Combine(BasePath, "dataset", "dataset_03.json");
+            Console.WriteLine($"Reading Dataset: {datasetPath}");
+            List<Sequence> sequences = HelperMethods.ReadDataset(datasetPath);
+
+
+        }
+    }
+
+}
