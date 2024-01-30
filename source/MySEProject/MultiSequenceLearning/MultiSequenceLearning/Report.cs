acccc24a8af0eae0d8eb90eb38fe78baa60116af
Saad Jamil <33003836+saadjamil5@users.noreply.github.com>
2024-01-30 23:25:30 +0100

    Update Report.cs

--- source/MySEProject/MultiSequenceLearning/MultiSequenceLearning/Report.cs
+++ source/MySEProject/MultiSequenceLearning/MultiSequenceLearning/Report.cs
@@ -1,12 +1,16 @@
-﻿using System;
+using Org.BouncyCastle.Asn1.Mozilla;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MultisequenceLearning
+namespace MultiSequenceLearning
 {
-    internal class Report
+    public class Report
     {
-    }
-}
+        public Report() { }
+
+        public string SequenceName { get; set; }
+        public int[] SequenceData { get; set; }
+}     
