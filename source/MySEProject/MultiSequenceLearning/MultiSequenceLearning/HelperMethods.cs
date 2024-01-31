ac4f154c2c8bed234115cbed1aa7cb3644b8917a
Saad Jamil <33003836+saadjamil5@users.noreply.github.com>
2024-02-01 00:36:04 +0100

    Update HelperMethods.cs

--- source/MySEProject/MultiSequenceLearning/MultiSequenceLearning/HelperMethods.cs
+++ source/MySEProject/MultiSequenceLearning/MultiSequenceLearning/HelperMethods.cs
@@ -1,8 +1,16 @@
-﻿using System;
+using System;
+using System.Globalization;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NeoCortexApi;
+using NeoCortexApi.Encoders;
+using NeoCortexApi.Entities;
+using MultiSequenceLearning;
+using Newtonsoft.Json;
+
 
 namespace MultisequenceLearning
 {
