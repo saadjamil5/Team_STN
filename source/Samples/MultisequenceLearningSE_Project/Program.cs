7b231e858261a844303892f3e3f6287f43c26d73
nabeelamaham <nabeela.maham@stud.fra-uas.de>
2024-02-17 17:13:22 +0100

    implemented logics to calculate accuracy

--- source/Samples/MultisequenceLearningSE_Project/Program.cs
+++ source/Samples/MultisequenceLearningSE_Project/Program.cs
@@ -1,4 +1,4 @@
-﻿using MultisequenceLearning;
+﻿using MultiSequenceLearning;
 using NeoCortexApi;
 using NeoCortexApi.Encoders;
 using Newtonsoft.Json;
@@ -93,6 +93,63 @@ namespace MultiSequenceLearning
 
             int prev = -1;
             bool first = true;
+
+            /*
+            * Pseudo code for calculating accuracy:
+            * 
+            * 1.      loop for each element in the sub-sequence
+            * 1.1     if the element is first element do nothing and save the element as 'previous' for further comparision
+            * 1.2     take previous element and predict the next element
+            * 1.2.1   get the predicted element with highest similarity and compare with 'next' element
+            * 1.2.1.1 if predicted element matches the next element increment the counter of matched elements
+            * 1.2.2   increment the count for number of calls made to predict an element
+            * 1.2     update the 'previous' element with 'next' element
+            * 2.      calculate the accuracy as (number of matched elements)/(total number of calls for prediction) * 100
+            * 3.      end the loop
+            */
+
+            foreach (var next in list)
+            {
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    Console.WriteLine($"Input: {prev}");
+                    var res = predictor.Predict(prev);
+                    string log = "";
+                    if (res.Count > 0)
+                    {
+                        foreach (var pred in res)
+                        {
+                            Debug.WriteLine($"Predicted Input: {pred.PredictedInput} - Similarity: {pred.Similarity}%");
+                        }
+
+                        var sequence = res.First().PredictedInput.Split('_');
+                        var prediction = res.First().PredictedInput.Split('-');
+                        Console.WriteLine($"Predicted Sequence: {sequence.First()} - Predicted next element: {prediction.Last()}");
+                        log = $"Input: {prev}, Predicted Sequence: {sequence.First()}, Predicted next element: {prediction.Last()}";
+                        //compare current element with prediction of previous element
+                        if (next == Int32.Parse(prediction.Last()))
+                        {
+                            matchCount++;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing predicted :(");
+                        log = $"Input: {prev}, Nothing predicted";
+                    }
+
+                    logs.Add(log);
+                    predictions++;
+                }
+
+                //save previous element to compare with upcoming element
+                prev = next;
+            }
+
         }
 
 
